74f4c0bd945e9a037e327a3a0bd39713b6a2e884
Andreas Olsen <Andreas.Olsen@visma.com>
2020-04-26 13:34:33 +0200

    Test moved to another folder

---
+++ Test/FibonacciTest/should_calculat_fibonacci.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTest
+{
+    [TestClass]
+    public class should_calculat_fibonacci
+    {
+        [TestMethod]
+        public void It_should_give_()
+        {
+
+        }
+    }
+}
